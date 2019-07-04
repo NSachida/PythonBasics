f649590a1fffe060710b3c343b337b75e09475cf
Asmaa Mirkhan <45575570+asmaamirkhan@users.noreply.github.com>
2019-07-04 12:51:12 +0300

    More on strings :mag:

--- GUIDE_string.py
+++ GUIDE_string.py
@@ -2,6 +2,8 @@
 
 # STRINGS
 
+# -----------------------------------
+
 # string declaration
 sample_string = 'asmaa'
 hello_string = "hello world"
@@ -14,6 +16,8 @@ print(multi_line)
 # Hello,
 # my name is asmaa
 
+# -----------------------------------
+
 # access chars (indexing) and slicing
 print('Accessing and slicing: ',
       hello_string[0], hello_string[1:3], hello_string[:5], hello_string[-1])
@@ -22,7 +26,9 @@ print('Accessing and slicing: ',
 # Note: string are immutable
 # hello_string[1] = 'f' # ERROR
 
-# string operations:
+# -----------------------------------
+
+# Arithmetic operators on strings
 my_string = hello_string + ' ' + sample_string
 print('+ op: ', my_string)
 
@@ -37,16 +43,22 @@ print(my_string)
 # hello worldhello worldhello world
 # helloworld
 
+# -----------------------------------
+
 # iterating enumarate(sttring)
 for i, letter in enumerate(my_string):
     print(i, letter)
 
 # Output: prints all chars in the string with indeces
 
+# -----------------------------------
+
 # membership test
 print('Membership: ', 'h' in my_string, 'm' in my_string)
 
 # Output: True Flase
+
+# -----------------------------------
 
 # string formatting
 my_string = 'name: {}, surname: {}'
@@ -54,6 +66,8 @@ my_string = my_string.format('asmaa', 'mirkhan')
 print('Formatting: ', my_string)
 
 # Output: name: asmaa, surname: mirkhan
+
+# -----------------------------------
 
 # char operations:
 print('Upper, lower:', my_string.upper(), my_string.lower())
@@ -67,6 +81,14 @@ print('Case swapping:', string.swapcase())
 
 # Output: aSMaa mIRkHAn
 
+# .title(): returns camel case string with spaces
+s = 'introduction to python'
+print('Title:', s.title())
+
+# Output: Introduction To Python
+
+# -----------------------------------
+
 # find(string) returns index of first matching substring
 print('Find find():', my_string.find('me'))
 
@@ -77,9 +99,13 @@ print('Find rfind():', my_string.rfind('me'))
 # 2
 # 18
 
+# -----------------------------------
+
 # splitting:
 splitted = my_string.split()
 print('Splitting:', splitted)
+
+# joining
 joined = ''.join(splitted)
 print('Joining', joined)
 
@@ -87,11 +113,32 @@ print('Joining', joined)
 # ['name:', 'asmaa,', 'surname:', 'mirkhan']
 # name:asmaa,surname:mirkhan
 
+# another example
+# declare a string
+original = 'My name is Asmaa'
+
+# convert string into list and splitting criteria is ' ' (space)
+split = original.split(' ')
+print('Splitting:', split)
+
+# rejoin string (convert list into string again) and 
+# separate words by ',' ==> this parameter can be any string 
+join = ','.join(split)
+print('Joining:', join)
+
+# Output:
+# ['My', 'name', 'is', 'Asmaa']
+# My,name,is,Asmaa
+
+# -----------------------------------
+
 # replacing:
 my_string = my_string.replace('asmaa', 'esma')
 print('Replacing:', my_string)
 
 # Output: name: esma, surname: mirkhan
+
+# -----------------------------------
 
 # filling and centering
 
@@ -100,11 +147,14 @@ print('Filling: ', my_string)
 
 # Output: ******name: esma, surname: mirkhan******
 
+# -----------------------------------
+
 # insensitive copmaring
 print('Casefold:', 'asmaa'.casefold() == 'ASmaa'.casefold())
 
 # Output: True
 
+# -----------------------------------
 
 # start control
 print('Start control:', 'asmaa'.startswith('aa'), 'asmaa'.startswith('asm'))
@@ -121,6 +171,8 @@ print('Start control with parameter:', 'asmaa'.startswith('sm', 1, 3))
 print('End control:', 'asmaa'.endswith('aa'))
 
 # Output: True
+
+# -----------------------------------
 
 # char type control
 print('Char control: ', 'aa11ss22'.isalnum(), '--sssaaa2222'.isalnum())
